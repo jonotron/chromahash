e1f12a02744b767af02db1b221203dc37a934565
Jonathan Bowers <jonotron@gmail.com>
2017-10-12 08:47:57 -0700

    cleanup some comments

--- index.js
+++ index.js
@@ -9,23 +9,31 @@ function djb2(str){
     return hash;
 }
 
+// simple modulo hash to convert an int to a number from 0 - 255 (or so)
 function to256ish(i) {
-  //return (Math.abs(i) % 47) * 5 // almost 256
-  return (Math.abs(i) % 251)
+  // djb2 uses 33 as it's prime... should we then use k = 25 (coprime to 33),
+  // m = 10 to get a 256ish number? k = 251 seems to work
+  const k = 251
+  const m = 1
+
+  return (Math.abs(i) % k) * m
 }
 
+// convert a digit (base 10) to hex (base 16) left padded with 0
 function toHex(d) {
   return  ("0"+(Number(d).toString(16))).slice(-2)
 }
 
 function chromahash(str) {
-  const rStr = 'red'
-  const gStr = 'green'
-  const bStr = 'blue'
+  // we need to prefix the string to ensure different values for r/g/b but also
+  // if we only suffix we seem to get clustering around purple and yellow
+  const rStr = 'red'   + str
+  const gStr = 'green' + str
+  const bStr = 'blue'  + str
 
-  const rHex = toHex(to256ish(djb2(rStr + str)))
-  const gHex = toHex(to256ish(djb2(gStr + str)))
-  const bHex = toHex(to256ish(djb2(bStr + str)))
+  const rHex = toHex(to256ish(djb2(rStr)))
+  const gHex = toHex(to256ish(djb2(gStr)))
+  const bHex = toHex(to256ish(djb2(bStr)))
 
   return `${rHex}${gHex}${bHex}`
 }
