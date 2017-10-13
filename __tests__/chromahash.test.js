229d2b2f9273f615d4c1607fba074544a89fbd4e
Jonathan Bowers <jonotron@gmail.com>
2017-10-12 20:07:27 -0700

    a few additional simple tests

--- __tests__/chromahash.test.js
+++ __tests__/chromahash.test.js
@@ -6,3 +6,18 @@ test('successive hashes are the same for a given word', () => {
 
   expect(a).toEqual(b)
 })
+
+test('different words return different colors', () => {
+  const a = chromahash('letterA')
+  const b = chromahash('letterB')
+
+  expect(a).not.toEqual(b)
+})
+
+test('colors are hex', () => {
+  const a = chromahash('someword')
+  const n = parseInt(a, 16)
+
+  // TODO: this is a pretty poor test. Surely we can do better.
+  expect(n).toBeGreaterThan(1)
+})
