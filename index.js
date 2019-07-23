46e23db9d932601fa10bc76c8108b4d2b82dd8e1
BEvans99 <bevans@bclc.com>
2019-07-23 15:52:28 -0700

    feat(index js): messing with colour distribution

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function djb2(str) {
   return hash
 }
 
+function mainColour(i) {
+  let colourFamilies = 6
+
+  return (Math.abs(i) % colourFamilies) + 1
+}
+
 // simple modulo hash to convert an int to a number from 0 - 255 (or so)
 function to256ish(i) {
   // djb2 uses 33 as it's prime... should we then use k = 25 (coprime to 33),
@@ -17,7 +23,7 @@ function to256ish(i) {
   const k = 5
   const m = 256 / k
 
-  return (Math.abs(i) % k) * m
+  return (Math.abs(i) % k) * 70
 }
 
 // convert a digit (base 10) to hex (base 16) left padded with 0
@@ -32,9 +38,37 @@ function chromahash(str) {
   const gStr = 'green' + str
   const bStr = 'blue' + str
 
+  let weightTowards = mainColour(djb2(str))
+
+  console.log(weightTowards)
+
+  let weightMatrix = { red: 0, green: 0, blue: 0 }
+
+  switch (weightTowards) {
+    case 1:
+      weightMatrix = { red: 1, green: 0, blue: 0 }
+      break
+    case 2:
+      weightMatrix = { red: 1, green: 1, blue: 0 }
+    case 3:
+      weightMatrix = { red: 0, green: 1, blue: 1 }
+      break
+    case 4:
+      weightMatrix = { red: 1, green: 0, blue: 1 }
+      break
+    case 5:
+      weightMatrix = { red: 0, green: 1, blue: 0 }
+    case 6:
+      weightMatrix = { red: 0, green: 0, blue: 1 }
+  }
+
+  console.log(weightMatrix)
+
   const rHex = toHex(to256ish(djb2(rStr)))
   const gHex = toHex(to256ish(djb2(gStr)))
   const bHex = toHex(to256ish(djb2(bStr)))
+
+  console.log(`${rHex}${gHex}${bHex}`)
 
   return `${rHex}${gHex}${bHex}`
 }
